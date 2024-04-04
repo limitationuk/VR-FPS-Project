a6f96c362f6d798fcff0bfacc1deca128f466a66
Hanjeonguk <wjddnr1358@naver.com>
2024-04-04 17:30:41 +0900

    feature: Player TakeDamage

--- Assets/Player/Scripts/Potal.cs
+++ Assets/Player/Scripts/Potal.cs
@@ -9,11 +9,8 @@ public class Potal : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(1 << other.gameObject.layer);
-        Debug.Log(LayerMask.GetMask("Player"));
         if (((1 << other.gameObject.layer) & LayerMask.GetMask("Player")) != 0)
         {
-            Debug.Log("1");
             Manager.Scene.LoadScene(scenename);
         }
     }
